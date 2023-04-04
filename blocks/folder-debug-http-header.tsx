58a3d65d6a78ac56cc1c6ea8a87c51b7d0d68b17
Marwan Zibaoui <marwan.zibaoui@shopify.com>
2023-04-04 12:06:00 +0200

    http header debug block

--- blocks/folder-debug-http-header.tsx
+++ blocks/folder-debug-http-header.tsx
@@ -1,25 +1,47 @@
 import { useCallback, useState } from "react";
 import { FolderBlockProps } from "@githubnext/blocks";
-import { Box, Button } from "@primer/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  Checkbox,
+  Spinner,
+  Flash,
+  Text,
+} from "@primer/react";
 
 export default function DebugHttpHeader(props: FolderBlockProps) {
-  const [headers, setHeaders] = useState<{ [key: string]: string }>({
-    "x-cache-key": "a",
-    "x-cache-status": "a",
-    "x-content-type-options": "a",
-    "x-worker-name": "a",
-    "x-worker-revision": "a",
-  });
+  const [headers, setHeaders] = useState<Array<Array<string>>>([]);
+  const [debug, setDebug] = useState<Boolean>(false);
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [error, setError] = useState();
 
   const sendRequest = useCallback(() => {
-    fetch("https://cors-proxy.rickymarou.workers.dev/https://shopify.com", {
-      // headers: {
-      //   "shopify-edge-cache-debug": "1",
-      // },
-    }).then((res) => {
-      console.log(...res.headers);
-    });
-  }, []);
+    setError(undefined);
+    setLoading(true);
+    fetch(
+      "https://cors-proxy.rickymarou.workers.dev/https://shopify.com",
+      debug
+        ? {
+            headers: {
+              "shopify-edge-cache-debug": "1",
+            },
+          }
+        : {}
+    )
+      .then((res) => {
+        console.log(...res.headers);
+        setHeaders(Array.from(res.headers));
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(e);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [debug]);
+
   return (
     <Box p={4}>
       <Box
@@ -36,40 +58,56 @@ export default function DebugHttpHeader(props: FolderBlockProps) {
           borderBottomStyle="solid"
           borderColor="border.default"
         >
-          Check the status of the CF cache from your region
+          Check the status of the CF cache. <br />
+          <Text fontSize={1} color="color.">
+            Request proxied through https://cors-proxy.rickymarou.workers.dev to
+            avoid CORS. Debug header not working 😞
+          </Text>
         </Box>
         <Box p={4}>
-          <Box paddingBottom={4}>
-            <Button onClick={sendRequest}>Send request</Button>
+          <Box paddingBottom={4} display="flex" alignItems="center">
+            <Box paddingRight={4}>
+              <Button onClick={sendRequest}>Send request</Button>
+            </Box>
+            <Box>
+              <FormControl>
+                <FormControl.Label>Add debug header</FormControl.Label>
+                <Checkbox checked={debug} onChange={() => setDebug(!debug)} />
+              </FormControl>
+            </Box>
           </Box>
-          <Box
-            bg="canvas.subtle"
-            borderWidth={1}
-            borderStyle="solid"
-            borderRadius={6}
-            p={3}
-          >
-            <table style={{ textAlign: "left" }}>
-              <thead>
-                <tr>
-                  <th className="p-1">header</th>
-                  <th className="p-1" style={{ paddingLeft: "48px" }}>
-                    value
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.keys(headers).map((headerKey, index) => (
-                  <tr key={index}>
-                    <td className="p-1">{headerKey}</td>
-                    <td className="p-1" style={{ paddingLeft: "48px" }}>
-                      {headers[headerKey]}
-                    </td>
+          {loading && <Spinner />}
+          {error && <Flash variant="danger">{String(error)}</Flash>}
+          {!loading && !error && (
+            <Box
+              bg="canvas.subtle"
+              borderWidth={1}
+              borderStyle="solid"
+              borderRadius={6}
+              p={3}
+            >
+              <table style={{ textAlign: "left" }}>
+                <thead>
+                  <tr>
+                    <th className="p-1">header</th>
+                    <th className="p-1" style={{ paddingLeft: "48px" }}>
+                      value
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </Box>
+                </thead>
+                <tbody>
+                  {headers.map((header, index) => (
+                    <tr key={index}>
+                      <td className="p-1">{header[0]}</td>
+                      <td className="p-1" style={{ paddingLeft: "48px" }}>
+                        {header[1]}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>
