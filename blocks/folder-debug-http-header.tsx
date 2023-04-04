96e2ff90c34be5cd651666102f614fafaa53173e
Marwan Zibaoui <marwan.zibaoui@shopify.com>
2023-04-04 11:37:09 +0200

    CORS ruining everything as usual

--- blocks/folder-debug-http-header.tsx
+++ blocks/folder-debug-http-header.tsx
@@ -1,7 +1,25 @@
+import { useCallback, useState } from "react";
 import { FolderBlockProps } from "@githubnext/blocks";
-import { Box } from "@primer/react";
+import { Box, Button } from "@primer/react";
 
 export default function DebugHttpHeader(props: FolderBlockProps) {
+  const [headers, setHeaders] = useState<{ [key: string]: string }>({
+    "x-cache-key": "a",
+    "x-cache-status": "a",
+    "x-content-type-options": "a",
+    "x-worker-name": "a",
+    "x-worker-revision": "a",
+  });
+
+  const sendRequest = useCallback(() => {
+    fetch("https://cors-proxy.rickymarou.workers.dev/https://shopify.com", {
+      // headers: {
+      //   "shopify-edge-cache-debug": "1",
+      // },
+    }).then((res) => {
+      console.log(...res.headers);
+    });
+  }, []);
   return (
     <Box p={4}>
       <Box
@@ -18,27 +36,40 @@ export default function DebugHttpHeader(props: FolderBlockProps) {
           borderBottomStyle="solid"
           borderColor="border.default"
         >
-          This is the folder content.
+          Check the status of the CF cache from your region
         </Box>
         <Box p={4}>
-          <table style={{ textAlign: "left" }}>
-            <thead>
-              <tr>
-                <th className="p-1">Path</th>
-                <th className="p-1">Size</th>
-                <th className="p-1">Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.tree.map((item, index) => (
-                <tr key={index}>
-                  <td className="p-1">{item.path}</td>
-                  <td className="p-1">{item.size}</td>
-                  <td className="p-1">{item.type}</td>
+          <Box paddingBottom={4}>
+            <Button onClick={sendRequest}>Send request</Button>
+          </Box>
+          <Box
+            bg="canvas.subtle"
+            borderWidth={1}
+            borderStyle="solid"
+            borderRadius={6}
+            p={3}
+          >
+            <table style={{ textAlign: "left" }}>
+              <thead>
+                <tr>
+                  <th className="p-1">header</th>
+                  <th className="p-1" style={{ paddingLeft: "48px" }}>
+                    value
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {Object.keys(headers).map((headerKey, index) => (
+                  <tr key={index}>
+                    <td className="p-1">{headerKey}</td>
+                    <td className="p-1" style={{ paddingLeft: "48px" }}>
+                      {headers[headerKey]}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Box>
         </Box>
       </Box>
     </Box>
