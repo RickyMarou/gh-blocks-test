2d37a4a485327544b039a10b86c45ad794af31f3
Marwan Zibaoui <marwan.zibaoui@shopify.com>
2023-04-06 10:59:51 +0200

    edit json render ok

--- blocks/edit-json-file.tsx
+++ blocks/edit-json-file.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { FolderBlockProps } from "@githubnext/blocks";
+import { FolderBlockProps, getLanguageFromFilename } from "@githubnext/blocks";
 import {
   Box,
   Button,
@@ -8,7 +8,9 @@ import {
   Spinner,
   Flash,
   Text,
+  TextInput,
 } from "@primer/react";
+import randomColor from "randomcolor";
 
 export default function EditJSONFile(props: FolderBlockProps) {
   const {
@@ -19,6 +21,14 @@ export default function EditJSONFile(props: FolderBlockProps) {
     onRequestBlocksRepos,
     onRequestGitHubData,
   } = props;
+
+  const language = getLanguageFromFilename(context.path.split("/").pop() || "");
+  let fileContent;
+  try {
+    fileContent = JSON.parse(content);
+  } catch (error) {}
+
+  console.log({ fileContent });
 
   return (
     <Box p={4}>
@@ -36,10 +46,56 @@ export default function EditJSONFile(props: FolderBlockProps) {
           borderBottomStyle="solid"
           borderColor="border.default"
         >
-          Create a new page
+          Edit JSON File
         </Box>
-        <Box p={4}></Box>
+        <Box p={4}>
+          {(language !== "JSON" || !fileContent) && (
+            <Flash variant="danger">Error parsing JSON</Flash>
+          )}
+          <RenderJSON fileContent={fileContent} />
+        </Box>
       </Box>
     </Box>
+  );
+}
+
+function RenderJSON({ fileContent }) {
+  return (
+    <div>
+      {Object.entries(fileContent).map((jsonEntry, i) => {
+        const key = jsonEntry[0];
+        const value = jsonEntry[1];
+
+        if (typeof value === "string") {
+          // stop case
+          return (
+            <FormControl sx={{ display: "flex", flexDirection: "row" }}>
+              <FormControl.Label sx={{ alignSelf: "center", flex: "0.2" }}>
+                {key}
+              </FormControl.Label>
+              <TextInput sx={{ flex: "1" }} value={value} />
+            </FormControl>
+          );
+        } else {
+          if (typeof value !== "object") {
+            return null;
+          }
+          const borderColor = randomColor();
+          return (
+            <Box
+              borderColor={borderColor}
+              borderWidth={1}
+              borderStyle="solid"
+              borderRadius={6}
+              padding={2}
+              marginTop={2}
+            >
+              <Box paddingBottom={2}>{key}</Box>
+              <RenderJSON fileContent={value} />
+            </Box>
+          );
+        }
+      })}
+    </div>
   );
 }
